d27cd705e82a71c0bea7d33406d8de8f5d8bb061
Vladimir Blinov <zemetrans21@yandex.ru>
2021-09-20 19:35:57 +0300

    feat: изменить модель user под ТЗ диплома

--- models/user.js
+++ models/user.js
@@ -8,17 +8,6 @@ const userSchema = new mongoose.Schema({
     maxlength: 30,
     default: "Жак-Ив Кусто",
   },
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: "Исследователь",
-  },
-  avatar: {
-    type: String,
-    default: "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
-    match: /^https?:\/\/(www.)?[a-zA-Z0-9-.]+\.[a-zA-Z]{2,}([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+)*#*$/,
-  },
   email: {
     type: String,
     required: true,
