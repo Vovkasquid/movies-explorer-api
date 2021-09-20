92072da56edfb1e1e3ab483d2265e33b87511358
Vladimir Blinov <zemetrans21@yandex.ru>
2021-09-21 02:58:33 +0300

    feat: изменить название БД на имя из чеклиста

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const auth = require("./middlewares/auth");
 const { PORT = 3002 } = process.env;
 
 // подключаемся к серверу mongo
-mongoose.connect("mongodb://localhost:27017/bitfilmsdb", {
+mongoose.connect("mongodb://localhost:27017/moviesdb", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
