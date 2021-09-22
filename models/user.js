3459532964e03c8cd7fe4ba7eb3690b8c1293582
Vladimir Blinov <zemetrans21@yandex.ru>
2021-09-22 21:17:06 +0300

    fix: добавить валидацию поля name в модель схемы users

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: 'Александр Петров',
+    required: true,
   },
   email: {
     type: String,
