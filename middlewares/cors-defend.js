2dd4b2783b21cfcc8a677850567ef238ca1252ab
Vladimir Blinov <zemetrans21@yandex.ru>
2021-11-17 16:51:44 +0300

    feat: обновить доменные имена в корс

--- middlewares/cors-defend.js
+++ middlewares/cors-defend.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'http://mestosquid.nomoredomains.rocks',
-  'https://mestosquid.nomoredomains.rocks',
+  'http://moviesquid.nomoredomains.monster',
+  'https://moviesquid.nomoredomains.monster',
   'http://localhost:3000',
   'https://localhost:3000',
 ];
